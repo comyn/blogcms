b14c23be9c9b6d1e79990b5b143657c2c5a287e3
comyn <xihatianwang2@163.com>
2021-01-12 11:43:56 +0800

    express:get,post,put,patch,delete请求

--- blogcms-api/app.js
+++ blogcms-api/app.js
@@ -1,10 +1,30 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 const port = 3000
 
-app.get('/', (req, res, next) => {
-  res.send('hello world')
+// 向客户端公开目录,等价于 app.use(express.static(path.join(__dirname, './public'))) 或 app.use('/static', express.static(path.join(__dirname, './public')))
+app.use('/static', express.static(path.join(__dirname, './public')))
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.post('/', function (req, res) {
+  res.send('Got a POST request')
+})
+
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user')
+})
+
+app.patch('/user', function (req, res) {
+  res.send('Got a PATCH request at /user')
+})
+
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user')
 })
 
 app.get('/api', (req, res, next) => {
@@ -16,9 +36,11 @@ app.get('/api', (req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
-  res.status(500).json({
-    error: err.message
-  })
+  console.error(err.stack)
+  res.status(500).send('Something broke!' + err.message)
+  // res.status(500).json({
+  //   error: err.message
+  // })
 })
 
 app.listen(port, () => {
