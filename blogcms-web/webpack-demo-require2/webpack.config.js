3efae5f3f4601da5a5f074382601145645dc9aa1
comyn <xihatianwang2@163.com>
2021-01-14 17:41:05 +0800

    动态修改bundle.js名称,保证每次不一样

--- blogcms-web/webpack-demo-require2/webpack.config.js
+++ blogcms-web/webpack-demo-require2/webpack.config.js
@@ -4,11 +4,11 @@ module.exports = {
   mode: "development", // "production" | "development" | "none"
   // 这里应用程序开始执行
   // webpack 开始打包
-  entry: './src/js/main.js', // string | object | array
+  entry: { bundle:'./src/js/main.js'}, // string | object | array
   output: { // webpack 如何输出结果的相关选项
     // 所有输出文件的目标路径
     // 必须是绝对路径（使用 Node.js 的 path 模块）
     path: path.resolve(__dirname, 'dist'), // string
-    filename:'bundle.js' // string
+    filename:'[name].[chunkhash:8].js' // string, 增加8位的哈希值, name为entry的bundle
   }
 }
